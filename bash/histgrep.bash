45002b109b1458d026b901db799cf7d8a194e6e5
Sophie Dawson <sophiecdawson@gmail.com>
2016-11-29 17:41:56 -0800

    Split bash history files into date based dirs
    This splits bash history into files based on date, time and process id so
    that the history can be huge. Took the histgrep.bash script from
    https://github.com/sjbach/env/blob/master/bin/histgrep.bash
    and adapted it slightly. The script works as an alternate way to search history
    instead of using Ctrl-R.

---
+++ bash/histgrep.bash
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# Grep through archived command history.
+# (See //cron/save-hist.bash)
+#
+
+extended_regex_flag='-r'
+if [ "`uname`" = "Darwin" ]; then
+  extended_regex_flag='-E'
+fi
+
+# Get results, reverse, prepend line numbers
+grep --text "$@" -- ~/.bash_history ~/.history/**/**/* | tac | nl | \
+#
+# Insert line numbers for better intra-day sorting
+sed $extended_regex_flag 's/([0-9]+) *([^:]*):(.*)/\2:\1:\3/' | \
+#
+# Pretty print
+sed $extended_regex_flag 's%.*\.history/(.*)\.(.*\.)?(histfile|bash_history):%\1:%' | \
+#
+# Put more recent lines first like this:
+#   year, month, day, then implicitly by the inserted line number
+# Do this now so that the unique sort prefers more recent lines.
+sort --key 1.7,1.8nr --key 1.1,1.3Mr --key 1.4,1.5nr | \
+#
+# Remove duplicates
+sort -u -t ':' --key 3 | \
+#
+# Put more recent lines first again (it gets screwed up)
+sort --key 1.7,1.8nr --key 1.1,1.3Mr --key 1.4,1.5nr # | \
+#
+# Remove the inserted line numbers.
+# sed $extended_regex_flag 's/:[0-9]+:/:/'
+
