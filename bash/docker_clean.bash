653cf7bfb5ba86bdeacbf46f52ae07489eeef4b0
Sophie Dawson <sophie@remind101.com>
2021-08-17 15:03:55 -0700

    Add cache-busting version of dockerclean

--- bash/docker_clean.bash
+++ bash/docker_clean.bash
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-# Cleanup unused images
-docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs docker rmi
+while getopts b flag
+do
+  case "${flag}" in
+    b) boom=true;;
+  esac
+done
 
-# Cleanup containers
-docker ps --filter status=dead --filter status=exited -aq | xargs docker rm -v
+if [ "$boom" = true ]
+then
+  docker kill $(docker ps -q)
+  docker rm $(docker ps --filter=status=exited --filter=status=created -q)
+  docker rmi $(docker images -a -q)
+else
+  # Cleanup unused images
+  docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs docker rmi
 
-# Cleanup unused volumes
-docker volume ls -qf dangling=true | xargs docker volume rm
+  # Cleanup containers
+  docker ps --filter status=dead --filter status=exited -aq | xargs docker rm -v
+
+  # Cleanup unused volumes
+  docker volume ls -qf dangling=true | xargs docker volume rm
+fi
